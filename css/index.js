0e0adc9762abe500dddd0a8ade90f2dc607f6cd9
samandar4eek <124165162+Samandar4eek-design@users.noreply.github.com>
2024-12-25 20:36:28 +0500

    page trafig

---
+++ css/index.js
@@ -0,0 +1,41 @@
+function toggleButtons() {
+    var button1 = document.getElementById('btn-1');
+    var button2 = document.getElementById('btn-2');
+
+    if (button1.style.display !== 'none') {
+        button1.style.display = 'none';
+        button2.style.display = 'inline-block';
+    }
+    else {
+        button2.style.display = 'none';
+        button1.style.display = 'inline-block';
+    }
+}
+
+function toggleButtonss() {
+    var button1 = document.getElementById('btnn-1');
+    var button2 = document.getElementById('btnn-2');
+
+    if (button1.style.display !== 'none') {
+        button1.style.display = 'none';
+        button2.style.display = 'block';
+    }
+    else {
+        button2.style.display = 'none';
+        button1.style.display = 'block';
+    }
+}
+
+function toggleButtonsss() {
+    var button1 = document.getElementById('btnnn-1');
+    var button2 = document.getElementById('btnnn-2');
+
+    if (button1.style.display !== 'none') {
+        button1.style.display = 'none';
+        button2.style.display = 'block';
+    }
+    else {
+        button2.style.display = 'none';
+        button1.style.display = 'block';
+    }
+}
